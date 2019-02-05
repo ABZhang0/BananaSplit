6ebe448de1cc9d2adf42706255f09f6248ff9930
Alan Zhang <a.b.zhang@outlook.com>
2019-02-05 00:25:57 -0800

    list bill activity implemented

--- app/src/main/java/com/example/billsplitapp/BillAdapter.java
+++ app/src/main/java/com/example/billsplitapp/BillAdapter.java
@@ -10,8 +10,8 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private ArrayList<Buyer> buyers;
+public class BillAdapter extends RecyclerView.Adapter<BillAdapter.MyViewHolder> {
+    private ArrayList<PurchaseItem> purchaseItems;
     private OnItemClickListener listener;
 
     public interface OnItemClickListener {
@@ -23,17 +23,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder  {
-        public ImageView imageView;
-        public TextView textView;
-        public TextView textView2;
+        public TextView textView3;
+        public TextView textView4;
         public ImageView deleteImage;
 
         public MyViewHolder(View view, final OnItemClickListener listener) {
             super(view);
-            imageView = view.findViewById(R.id.imageView);
-            textView = view.findViewById(R.id.textView);
-            textView2 = view.findViewById(R.id.textView2);
-            deleteImage = view.findViewById(R.id.imageView2);
+            textView3 = view.findViewById(R.id.textView3);
+            textView4 = view.findViewById(R.id.textView4);
+            deleteImage = view.findViewById(R.id.imageView3);
 
             deleteImage.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -49,28 +47,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         }
     }
 
-    MyAdapter(ArrayList<Buyer> buyers) {
-        this.buyers = buyers;
+    BillAdapter(ArrayList<PurchaseItem> purchaseItems) {
+        this.purchaseItems = purchaseItems;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.buyer_item, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.purchase_item, viewGroup, false);
         return new MyViewHolder(view, listener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder myViewHolder, int i) {
-        Buyer buyer = buyers.get(i);
-        myViewHolder.imageView.setImageResource(buyer.getPortrait());
-        myViewHolder.textView.setText(buyer.getName());
-        myViewHolder.textView2.setText(String.valueOf(buyer.getBill()));
+        PurchaseItem purchaseItem = purchaseItems.get(i);
+        myViewHolder.textView3.setText(purchaseItem.getName());
+        myViewHolder.textView4.setText(String.valueOf(purchaseItem.price()));
     }
 
     @Override
     public int getItemCount() {
-        return buyers.size();
+        return purchaseItems.size();
     }
-
 }
--- app/src/main/java/com/example/billsplitapp/ListBillActivity.java
+++ app/src/main/java/com/example/billsplitapp/ListBillActivity.java
@@ -1,22 +1,27 @@
 package com.example.billsplitapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import java.util.ArrayList;
 
 public class ListBillActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private EditText editText2;
+    private EditText editText3;
 
     private RecyclerView recyclerView;
-    private MyAdapter rvAdapter;
+    private BillAdapter rvAdapter;
     private RecyclerView.LayoutManager rvLayoutManager;
 
     private ArrayList<PurchaseItem> purchaseItems;
-
     private static ArrayList<Buyer> buyers;
 
     @Override
@@ -29,5 +34,54 @@ public class ListBillActivity extends AppCompatActivity {
         } catch (Exception e) {
             System.out.println(e.getLocalizedMessage());
         }
+
+        purchaseItems = new ArrayList<>();
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+        buildRecyclerView();
+    }
+
+    public void sendPurchaseItem(View view) {
+        String name = editText2.getText().toString();
+        String price = editText3.getText().toString();
+
+        if(!name.isEmpty() && !price.isEmpty()) {
+            purchaseItems.add(new PurchaseItem(name, Double.parseDouble(price)));
+            rvAdapter.notifyItemInserted(purchaseItems.size());
+        }
+
+        editText2.getText().clear();
+        editText3.getText().clear();
+        hideSoftKeyboard(this);
+    }
+
+    public void onFinish (View view) {
+        if (!purchaseItems.isEmpty()) {
+            //start calculating bill split in next activity
+        }
+    }
+
+    public void hideSoftKeyboard (Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                activity.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void buildRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView2);
+        rvLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(rvLayoutManager);
+        rvAdapter = new BillAdapter(purchaseItems);
+        recyclerView.setAdapter(rvAdapter);
+
+        rvAdapter.setOnItemClickListener(new BillAdapter.OnItemClickListener() {
+            @Override
+            public void onDeleteClick(int position) {
+                purchaseItems.remove(position);
+                rvAdapter.notifyItemRemoved(position);
+            }
+        });
     }
 }
